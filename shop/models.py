bab4494108b66fdeba0bf349049fd267afde4b5a
Vlad <vlad@exampl.com>
2025-11-13 16:18:56 +0200

    init project created detail product list product also created cart

---
+++ shop/models.py
@@ -0,0 +1,51 @@
+from django.db import models
+from django.urls import reverse
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse("shop:product_list_by_category", args=[self.slug])
+    
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['name']),
+        ]
+        verbose_name = 'category'
+        verbose_name_plural = 'categories'
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE, related_name='products')
+    slug = models.SlugField(max_length=100, unique=True)
+    image = models.ImageField(upload_to='products/%Y/%m/%d', blank=True)
+
+    description = models.TextField()
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+
+    available = models.BooleanField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['name']
+        indexes = [
+            models.Index(fields=['id', 'slug']),
+            models.Index(fields=['name']),
+            models.Index(fields=['-created'])
+        ]
+    
+    def __str__(self):
+        return self.name
+
+
+    def get_absolute_url(self):
+        return reverse("shop:product_detail", args=[self.id, self.slug])
